59fb09516943e3f4ad513a2ba2697a819bb418e2
Syarip mas'ud <32659666+usaidmasud@users.noreply.github.com>
2020-10-24 14:18:31 +0800

    counter component redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
 import Counter from "./containers/pages/Counter";
 import "./styles.css";
-
-const initialTodos = [
-  { id: 1, name: "React" },
-  { id: 2, name: "HTML" },
-  { id: 3, name: "SCSS" },
-  { id: 4, name: "Laravel" }
-];
+import { Provider } from "react-redux";
+import { store } from "./redux";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      todos: initialTodos
-    };
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>REDUX</h1>
-        <hr />
-        <Counter />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>REDUX</h1>
+          <hr />
+          <Counter />
+        </div>
+      </Provider>
     );
   }
 }
--- src/containers/pages/Counter/index.js
+++ src/containers/pages/Counter/index.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "../../../redux/actions/counterActions";
 
 class Counter extends React.Component {
   render() {
+    const { counter } = this.props.counter;
+    const { increment, decrement } = this.props;
     return (
       <div>
         <h2>Counter Component</h2>
-        <h3>{`Counter : `}</h3>
+        <h3>{`Counter : ${counter}`}</h3>
+
+        <div>
+          <button onClick={increment}>INCREMENT</button>
+          &nbsp;
+          <button onClick={decrement}>DECREMENT</button>
+        </div>
       </div>
     );
   }
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
